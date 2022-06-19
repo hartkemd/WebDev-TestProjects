cf3c16bdc5aebe361ce3e114171751028ad0b987
hartkemd <hartkemd@gmail.com>
2022-06-19 16:19:10 -0400

    Add objects test projects

---
+++ JavaScript/objects/test1/index.js
@@ -0,0 +1,7 @@
+function Crop(name, isAnnual, plantingDepth) {
+    this.name = name;
+    this.isAnnual = isAnnual;
+    this.plantingDepth = plantingDepth;
+}
+
+var peppermint = new Crop("peppermint", false, 2);
---
+++ JavaScript/objects/test2/index.js
@@ -0,0 +1,8 @@
+function Car(make, model, color, price) {
+    this.make = make;
+    this.model = model;
+    this.color = color;
+    this.price = price;
+}
+
+var corolla = new Car("Toyota", "Corolla", "gray", 15000);
